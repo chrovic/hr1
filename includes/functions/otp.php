1ae14f938c7a2b99c3d3bff3be7724febed1932b
cldnrio11 <rovic.castrodes@gmail.com>
2026-02-03 03:20:31 +0800

    aw

--- includes/functions/otp.php
+++ includes/functions/otp.php
@@ -1,7 +1,13 @@
 <?php
 require_once __DIR__ . '/../data/db.php';
 require_once __DIR__ . '/../../config/mailer.php';
-require_once __DIR__ . '/../../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../vendor/autoload.php';
+} else {
+    require_once __DIR__ . '/../lib/PHPMailer/Exception.php';
+    require_once __DIR__ . '/../lib/PHPMailer/PHPMailer.php';
+    require_once __DIR__ . '/../lib/PHPMailer/SMTP.php';
+}
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
